04e615261b2ea2be016a7b75b27dc5ac36230abf
Thuy Gia Nguyen <thuygia@gmail.com>
2025-03-10 11:05:57 +0100

    Updates

---
+++ _next/static/chunks/app/organizations/page-b5c3d3339ffb527d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[30,195,143,83,12,435,702,552,921,127,240],{4099:function(e,n,s){Promise.resolve().then(s.bind(s,5538)),Promise.resolve().then(s.bind(s,6512)),Promise.resolve().then(s.bind(s,2545))}},function(e){e.O(0,[722,538,971,117,744],function(){return e(e.s=4099)}),_N_E=e.O()}]);
