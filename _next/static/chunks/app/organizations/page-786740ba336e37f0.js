31d067a85525dfc28bfbe8bc8d20dd785f245bf0
Thuy Gia Nguyen <thuygia@gmail.com>
2025-03-10 19:42:35 +0100

    Updates

--- _next/static/chunks/app/organizations/page-786740ba336e37f0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[30,143,195,702,12,83,435,552,127,240,921],{4099:function(e,n,s){Promise.resolve().then(s.bind(s,5538)),Promise.resolve().then(s.bind(s,6512)),Promise.resolve().then(s.bind(s,2545))}},function(e){e.O(0,[590,153,468,314,777,538,971,117,744],function(){return e(e.s=4099)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/travel/page-f133ff2f3ce6c14f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[921,143,702,195,12,435,83,127,30,552,240],{4099:function(e,n,s){Promise.resolve().then(s.bind(s,5538)),Promise.resolve().then(s.bind(s,6512)),Promise.resolve().then(s.bind(s,2545))}},function(e){e.O(0,[590,153,468,358,777,538,971,117,744],function(){return e(e.s=4099)}),_N_E=e.O()}]);
